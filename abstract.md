d91f8ec23510693b8708979f3bd44dbbd1c7858a
Niels Hoffmann <n.hoffmann@geonovum.nl>
2025-05-20 10:23:28 +0200

    initieel wijzigingsprotocol

--- abstract.md
+++ abstract.md
@@ -1,1 +1,6 @@
 TODO: vul in abstract.md een abstract in.
+
+<aside class="note">
+NH: Dit is een eerste opzet van het wijzigingsprotocol voor DCAT-AP-NL.
+Dit document is nog niet compleet!
+</aside>
--- js/config.js
+++ js/config.js
@@ -1,79 +1,50 @@
-let respecConfig = {
-  useLogo: true,
-  useLabel: true,
-  // title is verplicht! Neem hier de titel van het document op ----------------------
-  title: "[TODO: Neem titel op in config.js]",
-  //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
+var documentConfig = {
+  title: "Wijzigingsprotocol DCAT-AP-NL",
   specStatus: "wv",                   // Werkversie
-  //specStatus: "cv",               // Consultatieversie
-  //specStatus: "vv",               // Versie ter vaststelling
-  //specStatus: "def",              // Vastgestelde versie
-  //specStatus: "basis",            // Basis Document
-  //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
-  //specType: "NO",                 // Norm
-  //specType: "ST",                 // Standaard
-  //specType: "IM",                 // Informatie Model
-  //specType: "PR",                 // Praktijkrichtlijn
   specType: "HR",                     // HandReiking
-  //specType: "WA",                 // Werkafspraak
-  //specType: "BD",                 // Beheer Documentatie
-  //specType: "AL",                 // Algemeen document
-  //specType: "BP",                 // Best Practice
-  //-- pubDomain is verplicht! (komt in de URL) -------------------------------------
-  //-- zie: https://geonovum.github.io/handleiding-tooling/ReSpec/#pubdomain
-  //-- TODO: vul pubDomain in
-  pubDomain: "wp",
-  //-- license: voor de geldende gebruiksvoorwaarden. Default is cc-by.
-  //license: "cc-by-nd",            // bronvermelding, geen afgeleide werken (default)
-  //license: "cc0",                 // Public Domain Dedication
+  pubDomain: "dcat",
   license: "cc-by",                 // Attribution, met bronvermelding
-  //-- TODO shortName is verplicht! (komt in de URL: kies logische afkorting)--------------
-  shortName: "NL-ReSpec-GN-template",
+  shortName: "dcat-ap-nl-wijzigingsprotocol",
   
-  //edDraftURI = De URI van de draft version. Deze wordt automatisch afgeleid van de github URI; maar kan hier overschreven worden. 
-	//edDraftURI: ["https://geonovum.github.io", "/", "shortName"],
-
-  //-- publishDate is verplicht -------------------------------------------------------
-  //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2023-03-28",
-  //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
-  //publishVersion: "0.0.2",
-  //previousVersion: "0.0.1",
-  //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  //previousPublishDate: "2014-05-01",
-  //previousMaturity: "CV",
-  //-- TODO: de namen van de Editor(s) / Redacteur(en)---------------------------------------
-  //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors:
     [
       {
-        name: "voornaam achternaam",
-        company: "Geonovum",
-        companyURL: "https://www.geonovum.nl",
-      }
+      name: "Ine de Visser",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    },
+    {
+      name: "Niels Hoffmann",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
     ],
   //-- de namen van de auteur(s) ------------------------------------------------------
   //-- vul in: per auteur: name:, company:, companyURL: -------------------------------
   authors:
     [
       {
-        name: "voornaam achternaam",
-        company: "Geonovum",
-        companyURL: "https://www.geonovum.nl",
-      }
+      name: "Ine de Visser",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    },
+    {
+      name: "Niels Hoffmann",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
     ],
   // TODO: Vul de github URL in.
   //neem hier de URL van de github repository op waar het respec document in staat
-  github: "https://github.com/Geonovum/NL-ReSpec-GN-template",
-  // Create PDF and link to file in header (optional):
-  // TODO: Change the filename as preferred.
-  alternateFormats: [
-      {
-          label: "pdf",
-          uri: "template.pdf",
-      },
-  ],
-  // Lokale lijst voor bibliografie
+  github: "https://github.com/Geonovum/dcat-ap-nl-wijzigingsprotocol",
+  labelColor: {
+        def: "#045D9F",
+        wv: "#FF0000",
+        cv: "#045D9F",
+        vv: "#045D9F",
+        basis: "#80CC28",
+    },
+   // Lokale lijst voor bibliografie
   // - Kijk eerst naar de beschikbare www.specref.org .
   // - Kijk daarna in de organisatieconfig.
   // - Voeg dan pas hieronder toe.
